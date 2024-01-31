6b2fbd3dfc1fc20b4196e3ebaef581dd998f46e3
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2024-01-31 19:49:30 +0100

    handcheck

--- src/pages/Step2.jsx
+++ src/pages/Step2.jsx
@@ -1,85 +1,89 @@
-import * as handTrack from 'handtrackjs';
-import { useRef, useState } from 'react';
+// import * as handTrack from 'handtrackjs';
+import { useEffect, useRef, useState } from "react";
+
+const modelParams = {
+  flipHorizontal: true, // flip e.g for video
+  maxNumBoxes: 20, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.6, // confidence threshold for predictions.
+};
 
 const Step2 = () => {
-    const videoRef = useRef(null)
-    const canvasRef = useRef(null)
-    const contextRef = useRef(null)
-    let trackButtonRef = useRef(null)
-    const btnSelectRef = useRef(null)
-    const cursorRef = useRef(null)
-    const [isVideo, setIsVideo] = useState(false)
-    const [model, setModel] = useState(null)
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [model, setModel] = useState(null);
+  const [contextState, setContextState] = useState(null);
 
-    const modelParams = {
-        flipHorizontal: true,   // flip e.g for video  
-        maxNumBoxes: 20,        // maximum number of boxes to detect
-        iouThreshold: 0.5,      // ioU threshold for non-max suppression
-        scoreThreshold: 0.6,    // confidence threshold for predictions.
-    }
+  useEffect(() => {
+    // Load the model.
+    handTrack.load(modelParams).then((lmodel) => {
+      // detect objects in the image.
+      setModel(lmodel);
+    });
+  }, []);
 
-    const startVideo = () => {
-        handTrack.startVideo(videoRef).then(function (status) {
-            console.log("video started", status);
-            if (status) {
-                setIsVideo(true)
-                runDetection()
-                console.log(" starting video");
-            } else {
-                console.log("Error starting video");
-            }
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    setContextState(context);
+  }, [canvasRef]);
+
+  const startVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: {} })
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+          handTrack
+            .startVideo(videoRef.current)
+            .then((status) => console.log("video started", status));
+        }
+      })
+      .catch((err) => console.error("err", err));
+  };
+
+  const runDetection = () => {
+    if (model) {
+      model.detect(videoRef.current).then((predictions) => {
+        model.renderPredictions(
+          predictions,
+          canvasRef.current,
+          contextState,
+          videoRef.current
+        );
+
+        predictions.forEach((prediction) => {
+          if (prediction.label !== "face") {
+            const [x, y, width, height] = prediction.bbox;
+            console.log(
+              `Main détectée à x: ${x}, y: ${y}, largeur: ${width}, hauteur: ${height}`
+            );
+          }
         });
+
+        requestAnimationFrame(runDetection);
+      });
     }
+  };
 
-    const toggleVideo = () => {
-        if (!isVideo) {
-            startVideo();
-        } else {
-            handTrack.stopVideo(video)
-            setIsVideo(false);
-        }
-    }
-    
-    const runDetection = () => {
-        console.log("running detection");
-        if(model){
-            model.detect(video).then(predictions => {
-                // console.log("Predictions: ", predictions);
-                model.renderPredictions(predictions, canvas, context, video);
-        
-                predictions.forEach(prediction => {
-                    if (prediction.label !== 'face') { // Assurez-vous que la classe 'hand' est correcte
-                        console.log(prediction)
-                        const [x, y, width, height] = prediction.bbox;
-                        console.log(`Main détectée à x: ${x}, y: ${y}, largeur: ${width}, hauteur: ${height}`);
-        
-                        moveCursor(prediction.bbox, cursor)
-                        checkCollision(prediction)   
-                    }
-                });
-        
-                if (isVideo) {
-                    requestAnimationFrame(runDetection);
-                }
-            });
-        }
-    }
+  return (
+    <div>
+      <div className="mb10">
+        <button onClick={startVideo}>Start video</button>
+      </div>
 
-    return ( 
-        <div>
-            <button className="btn" ref={btnSelectRef}>Click me</button>
-            <div id="cursor" ref={cursorRef}></div>
-            <div className="mb10">
-                <button onClick={toggleVideo} ref={trackButtonRef} className="bx--btn bx--btn--secondary" type="button">
-                    Toggle Video
-                </button>
-            </div>
-            <video className="videobox canvasbox" autoPlay id="myvideo" ref={videoRef}></video>
+      <video
+        ref={videoRef}
+        width="720"
+        height="560"
+        autoPlay
+        muted
+        className="videobox canvasbox"
+        onPlay={runDetection}
+      ></video>
 
-            <canvas ref={canvasRef} className="border canvasbox"></canvas>
+      <canvas ref={canvasRef} className="border canvasbox"></canvas>
+    </div>
+  );
+};
 
-        </div>
-     );
-}
- 
 export default Step2;
